20e8ea73bcc752e6bdc05215aa7ad5c5d9983c27
y321 <y1393649980@163.com>
2018-06-23 12:40:22 +0800

    first

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <el-container>
+    <el-header class="header" height="60px">
+      <h3>我的网站</h3>
+    </el-header>
+    <el-main>
+      <router-view/>
+    </el-main>
+  </el-container>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+body{
+  margin: 0;
+  padding: 0;
+}
+.el-header{
+ background-color: #409EFF;
+    color: white;
+  
+}
+</style>
---
+++ src/components/main.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>主页面</h1>
+        欢迎!<b @click="login">点这里登录</b>
+    </div>
+</template>
+
+<script>
+    export default {
+        methods: {
+            login () {
+                this.$router.replace('/login')
+            }
+        }
+    }
+</script>
