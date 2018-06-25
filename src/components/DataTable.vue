c8a4585b7cfeca37992e2fb60ae1114fd89fa64a
y321 <y1393649980@163.com>
2018-06-26 07:56:59 +0800

    提交

--- src/components/DataTable.vue
+++ src/components/DataTable.vue
@@ -113,9 +113,6 @@ export default{
             filterDates: null,  // 新增
             statuses: ['未开始', '进行中', '搁置', '完成'],
             statusColors: ['info', 'primary', 'warning', 'success'],
-            // 排序
-            sortProp: '',
-            sortOrder: '',
             // 分页
             currentPage: 1,
             currentPageSize: 3,
@@ -137,11 +134,7 @@ export default{
                 message: err
             }))
         },
-        // 排序
-        sortChange(column) {
-            this.sortProp = column.prop
-            this.sortOrder = column.order
-        },
+        
         // fenye
         pageSizeChange(size) {
             this.currentPageSize = size
@@ -234,34 +227,14 @@ export default{
         },
         // 排序
         sortedData() {
-            if (!this.sortOrder || !this.sortProp || !this.filtedData || !this.filtedData.length) return this.filtedData
-            var reverse = this.sortOrder == 'descending' ? -1 : 1
-            switch (typeof this.filtedData[0][this.sortProp]) {
-                case 'number':
-                    return this.filtedData.sort((a, b) => {
-                        return reverse * (a[this.sortProp] - b[this.sortProp])
-                    })
-                case 'string':
-                    if (JSON.stringify(new Date(this.filtedData[0][this.sortProp])) !== 'null') {
-                        return this.filtedData.sort((a, b) => {
-                            return reverse * (new Date(a[this.sortProp]) - new Date(b[this.sortProp]))
-                        })
-                    } else {
-                        return this.filtedData.sort((a, b) => {
-                            var cmp = 0
-                            if (a[this.sortProp] > b[this.sortProp]) cmp = 1
-                            else if (a[this.sortProp] < b[this.sortProp]) cmp = -1
-                            return reverse * cmp
-                        })
-                    }
-            }
+        
         },
         // fenye
         total() {
             return this.filtedData.length
         },
         pagedData() {
-            return this.sortedData.slice((this.currentPage - 1) * this.currentPageSize, this.currentPage * this.currentPageSize)
+            return this.filtedData.slice((this.currentPage - 1) * this.currentPageSize, this.currentPage * this.currentPageSize)
         }
     }
 }
